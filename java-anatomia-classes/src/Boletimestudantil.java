b4be3638d131ed988efc5ac03fa7cca2d685db13
rafael aguiar <rafaelqueiroz12@gmail.com>
2024-05-15 20:00:58 -0300

    primeiras aulas.

---
+++ java-anatomia-classes/src/Boletimestudantil.java
@@ -0,0 +1,12 @@
+public class Boletimestudantil {
+    
+    public static void main(String[] args) {
+        int mediaFinal = 10;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA DE MINERVA");
+        else
+            System.out.println("APROVADO");
+  }
+}
